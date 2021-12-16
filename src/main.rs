5ecc96db21c79e323264eb6a63a6c075fc423de4
DL Guilford <daniel@guilford.io>
2021-12-16 00:37:31 -0500

    first steps in rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
+use std::io::BufReader;
+use std::io;
+use std::path::Path;
+use std::io::Seek;
+use jwalk::{WalkDir};
+use std::io::Write;
 
-fn main() {
-    let mut f = File::open(filename).expect("file not found");
+fn add_file(p: &Path, encoder: &mut dyn Write) -> io::Result<()> {
+    let stat = fs::metadata(p)?;
+    if stat.is_file() && p != Path::new("./foo.cassette") {
+        let file = File::open(p)?;
+        let mut hasher = blake3::Hasher::new();
+        let mut reader = BufReader::new(file);
+        std::io::copy(&mut reader, &mut hasher)?;
+        reader.rewind()?;
+        std::io::copy(&mut reader, encoder)?;
+        let hash = hasher.finalize();
+        println!("{}: {}", hash, p.display()); 
+    }
+    Ok(())
 }
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let dir = args.get(1);
+    let archive = File::create("foo.cassette")?;
+    let mut frame_info = lz4_flex::frame::FrameInfo::new();
+    frame_info.block_mode = lz4_flex::frame::BlockMode::Linked;
+    let mut compressor = lz4_flex::frame::FrameEncoder::with_frame_info(frame_info, archive);
+
+    let mut n: u32 = 0;
+    for f in WalkDir::new(Path::new(dir.unwrap_or(&String::from(".")))) {
+	    add_file(f?.path().as_path(), &mut compressor)?;
+        n = n + 1;
+    }
+    println!("hashed {} files",  n);
+    Ok(())
+}
+
